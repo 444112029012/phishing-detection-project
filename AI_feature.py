867f31fe883490c3b8c3dfe6cf065a86117fefce
Yu <eason931234@gmail.com>
2026-03-18 13:36:07 +0800

    力行更新

--- AI_feature.py
+++ AI_feature.py
@@ -107,7 +107,7 @@ class Dataset_Manager:
             df_process = self.df.copy()
             
             for index, row in df_process.iterrows():
-                if index < 30000:
+                if index < 35000:
                     continue
                 elif index == 15230:
                     continue
